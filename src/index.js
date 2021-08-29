73f60546b1dc5084a32c505aa0e643783e3d5870
diogo <diogo.miguel.fonseca@gmail.com>
2021-08-29 23:42:34 +0100

    feat: eslint/prettier config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import './index.css'
-import { Board } from './components/board'
-import configureStore from './store'
-import { Provider } from 'react-redux'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@emotion/react';
+import './index.css';
+import { Board } from './components/board/Board';
+import configureStore from './store';
+import { theme } from './styles/theme';
 
-const store = configureStore()
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Board />
+      <ThemeProvider theme={theme}>
+        <Board />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
-)
+  document.getElementById('root'),
+);
---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    background: '#FFFFFF',
+    primary: '#3C6E71',
+    primaryAccent: '#2A4E50',
+    secondary: '#FFFCCA',
+    secondaryAccent: '#FFFBAD',
+  },
+  typography: {
+    paragraph: ['1.2rem', '1.4rem'],
+    board: ['4rem', '8rem'],
+  },
+  media: {
+    breakpoints: {
+      s: `@media screen and (min-width: 480px)`,
+    },
+  },
+};
