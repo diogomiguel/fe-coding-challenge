9d5dda17f81f2d621ed641665ba86f16c805f832
diogo <diogo.miguel.fonseca@gmail.com>
2021-08-30 14:27:41 +0100

    Revert "feat: game info and reset game"
    This reverts commit bfb350dd955334a99b5f3593199cb65b8ea772db.

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -3,23 +3,19 @@ import { useSelector } from 'react-redux';
 import { BoardCell } from './BoardCell';
 import { BoardGrid } from './BoardGrid';
 import { selectBoard } from '../../store/selectors/board';
-import { selectHasGameEnded } from '../../store/selectors/game';
-
-import { BOARD_MOVES } from '../../constants/board';
 
 export const Board = () => {
   const board = useSelector(selectBoard);
-  const hasGameEnded = useSelector(selectHasGameEnded);
 
   return (
-    <BoardGrid numCells={BOARD_MOVES}>
+    <BoardGrid>
       {board.map((rows, rowIndex) =>
         rows.map((player, colIndex) => (
           <BoardCell
             key={`${rowIndex}${colIndex}`}
             row={rowIndex}
             col={colIndex}
-            disabled={!!player || hasGameEnded}
+            disabled={!!player}
           >
             {player}
           </BoardCell>
